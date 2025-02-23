fb1a0b4aea812d90be1a9d174e21b03a0091efb3
soumili-03 <soumili.biswas3183@gmail.com>
2025-02-23 19:11:40 +0530

    removed redundant sub directory viteproject

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import pages and components
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import BubbleCursor from "./components/BubbleCursor";
+import Home from "./pages/Home.jsx";
+import Navbar from "./components/Navbar.jsx";
+import BubbleCursor from "./components/BubbleCursor.jsx";
 import SignIn from "./pages/SignIn/SignIn.jsx";
 import SignUp from "./pages/SignUp/SignUp.jsx";
 
