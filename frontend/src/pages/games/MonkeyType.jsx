92587813ceede71ac7bf8a06b4f0507ef79c7150
ShilpiBiswal <biswalshilpi@gmail.com>
2025-04-26 00:58:42 +0530

    genai integration

--- frontend/src/pages/games/MonkeyType.jsx
+++ frontend/src/pages/games/MonkeyType.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import MonkeyTypeComponent from '../../components/games/monkeytype/MonkeyTypeComponent'
+import { useParams } from "react-router-dom";
 
 const MonkeyType = () => {
+  const params = useParams();
+  const storedAgeGroup = localStorage.getItem("userAgeGroup");
+  const ageGroup = params.ageGroup || storedAgeGroup || "8-10";
   return (
     <div>
-      <MonkeyTypeComponent />
+      <MonkeyTypeComponent key={ageGroup} ageGroup={ageGroup} />
     </div>
   )
 }
