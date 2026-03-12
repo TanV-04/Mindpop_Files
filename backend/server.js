e2faabf3fd0d18b6ce02cbc0a6bafc486eed187a
ShilpiBiswal <biswalshilpi@gmail.com>
2026-03-12 11:39:07 +0530

    fix CSP policy

--- backend/server.js
+++ backend/server.js
@@ -53,15 +53,32 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
+
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "https://cdn.jsdelivr.net"
+        ],
+
+        styleSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "https://cdn.jsdelivr.net"
+        ],
+
         imgSrc: [
           "'self'",
           "data:",
+          "https:",
           "https://vincentgarreau.com"
         ],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-      },
-    },
+
+        connectSrc: [
+          "'self'",
+          "http://localhost:8001"
+        ]
+      }
+    }
   })
 );
 app.use(mongoSanitize());
